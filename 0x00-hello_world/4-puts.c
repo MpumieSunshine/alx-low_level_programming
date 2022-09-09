e07c6e8080700349d7683688da59ee7131fba07b
MpumieSunshine <mkhasibenompumelelo@gmail.com>
2022-09-08 20:39:26 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
+#include <stdio.h>
 /**
-*main-entry point
-*
-*Return:0 after printing
-*
-*/
+ * main - main block
+ * Return: 0
+ */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle"):
-return(0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
