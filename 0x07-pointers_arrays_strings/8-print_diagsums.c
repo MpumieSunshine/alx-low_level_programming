a81b1a2f151c4dc318863ef9ce649613ce071203
MpumieSunshine <mkhasibenompumelelo@gmail.com>
2022-09-26 18:37:06 -0700

    diagrams

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -7,15 +7,15 @@
 * @a: input pointer
 * @size: size of the matrix
 *
-* Return: nothing
+* @size: size of the matrix
 */
 
 void print_diagsums(int *a, int size)
 {
-int i, S1, S2; 
+int i, S1, S2;
 
-s1 = 0;
-s2 = 0;
+S1 = 0;
+S2 = 0; 
 
 for (i = 0; i < (size * size); i++)
 {
@@ -26,4 +26,3 @@ S2 += a[i];
 }
 printf("%d, %d\n", S1, S2);
 }
-
