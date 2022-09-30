225d8c966ac941204f7eee272c8417b15afeb9b3
MpumieSunshine <mkhasibenompumelelo@gmail.com>
2022-09-30 15:14:13 -0700

    myname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+  * main - this program prints its name
+  *
+  * @argc: number of arguments
+  * @argv: string of arguments
+  *
+  * Return: 0
+  */
+int main(int argc attribute((unused)), char *argv[])
+{
+ printf("%s\n", argv[0]);
+ return (0);
+}
