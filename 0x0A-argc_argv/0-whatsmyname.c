eab4d3531d0654b0e2fe9f58640ba29cab25bf5e
MpumieSunshine <mkhasibenompumelelo@gmail.com>
2022-09-30 15:47:56 -0700

    myname

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,8 +9,9 @@
   *
   * Return: 0
   */
-int main(int argc attribute((unused)), char *argv[])
+
+int main(int_argc attribute_((unused)), char *argv[])
 {
- printf("%s\n", argv[0]);
- return (0);
+ 	printf("%s\n", argv[0]);
+ 	return (0);
 }
