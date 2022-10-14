af715ec86204237f2df597f957b1557255b42652
MpumieSunshine <mkhasibenompumelelo@gmail.com>
2022-10-14 03:11:42 -0700

    100

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -23,9 +23,9 @@ void print_opcodes(char *a, int n)
 	printf("\n");
 }
 /**
-  * main - prints the opcodes of its own main function
-  * @argc: number of arguments passed to the function
-  * @argv: array of pointers to arguments
+ * main - prints the opcodes of its own main function
+ * @argc: number of arguments passed to the function
+ * @argv: array of pointers to arguments
  * Return: always O
  */
 int main(int argc, char **argv)
