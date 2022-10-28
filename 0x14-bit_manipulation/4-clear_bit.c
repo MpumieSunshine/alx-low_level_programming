3d24a5de744cb70f152fcf946d7c703f028fdaee
MpumieSunshine <mkhasibenompumelelo@gmail.com>
2022-10-27 17:51:06 -0700

    binary

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -4,7 +4,6 @@
  * clear_bit - sets the value of a bit to 0 at a given index
  * @n: parameter
  * @index: index
- *
  * Return: 1 if works, -1 if error
  */
 int clear_bit(unsigned long int *n, unsigned int index)
